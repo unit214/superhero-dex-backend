ed8647a59d45d30ab79ce4a8999417ba851643da
Petar Peshev <petar.peshev@leanplum.com>
2022-05-06 21:10:27 +0300

    ci: dockerfile entrypoint to expose db url, move all docker files in directory

---
+++ docker/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -e
+export DATABASE_URL="postgresql://$POSTGRES_USERNAME:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DATABASE?schema=$POSTGRES_SCHEMA"
+npm run serve:prod
