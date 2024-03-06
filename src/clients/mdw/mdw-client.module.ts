84a18221b280233eacfd4bfb0398f6ee2fac950c
Timo Erdelt <timo@unit214.de>
2024-03-06 13:28:56 +0100

    feat: add scheduling, resolve todos, cleanup & improve

--- src/clients/mdw/mdw-client.module.ts
+++ src/clients/mdw/mdw-client.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MdwClientService } from './mdw-client.service';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule],
   providers: [MdwClientService],
   exports: [MdwClientService],
 })
