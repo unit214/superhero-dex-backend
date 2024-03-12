cad05b96ae86becc809705be19ad83c6f071343f
Timo Erdelt <timo@unit214.de>
2024-03-12 15:55:21 +0100

    refactor(importer): thoroughly use Encoded string types everywhere

--- src/clients/mdw-client.model.ts
+++ src/clients/mdw-client.model.ts
@@ -1,3 +1,11 @@
+import {
+  AccountAddress,
+  ContractAddress,
+  KeyBlockHash,
+  MicroBlockHash,
+  TxHash,
+} from '../lib/utils';
+
 export type MdwPaginatedResponse<T> = {
   next?: string;
   prev?: string;
@@ -6,21 +14,21 @@ export type MdwPaginatedResponse<T> = {
 
 export type Contract = {
   aexn_type: string;
-  block_hash: string;
-  contract: string;
-  source_tx_hash: string;
+  block_hash: MicroBlockHash;
+  contract: ContractAddress;
+  source_tx_hash: TxHash;
   source_tx_type: string;
   create_tx: any;
 };
 
 export type ContractLog = {
   args: string[];
-  block_hash: string;
+  block_hash: MicroBlockHash;
   block_time: string;
-  call_tx_hash: string;
+  call_tx_hash: TxHash;
   call_txi: string;
-  contract_id: string;
-  contract_tx_hash: string;
+  contract_id: ContractAddress;
+  contract_tx_hash: TxHash;
   contract_txi: string;
   data: string;
   event_hash: string;
@@ -31,30 +39,30 @@ export type ContractLog = {
   height: string;
   log_idx: string;
   micro_index: string;
-  parent_contract_id: string;
+  parent_contract_id: ContractAddress;
 };
 
 export type AccountBalance = {
-  account: string;
+  account: AccountAddress;
   amount: string;
-  contract: string;
+  contract: ContractAddress;
 };
 
 export type BalancesV1 = {
-  amounts: Record<string, string>;
-  block_hash: string;
-  contract_id: string;
+  amounts: Record<AccountAddress, string>;
+  block_hash: MicroBlockHash;
+  contract_id: ContractAddress;
   height: string;
 };
 
 export type MdwMicroBlock = {
   micro_block_index: string;
   transactions_count: string;
-  hash: string;
+  hash: MicroBlockHash;
   height: string;
   pof_hash: string;
-  prev_hash: string;
-  prev_key_hash: string;
+  prev_hash: MicroBlockHash;
+  prev_key_hash: KeyBlockHash;
   signature: string;
   state_hash: string;
   time: string;
