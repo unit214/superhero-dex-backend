dd4c75c6ac4e271ed1974bdbaeec24d007aa973a
Timo Erdelt <timo@unit214.de>
2024-03-13 14:04:50 +0100

    feat(importer): fetch only as many pages as needed

--- src/clients/mdw-client.service.ts
+++ src/clients/mdw-client.service.ts
@@ -17,19 +17,30 @@ import {
 
 @Injectable()
 export class MdwClientService {
-  private LIMIT = 100;
-  private DIRECTION = 'forward';
-  private INT_AS_STRING = true;
-  private params = `direction=${this.DIRECTION}&limit=${this.LIMIT}&int-as-string=${this.INT_AS_STRING}`;
+  private readonly LIMIT = 100;
+  private readonly DIRECTION = 'forward';
+  private readonly INT_AS_STRING = true;
+  private readonly defaultParams = `direction=${this.DIRECTION}&limit=${this.LIMIT}&int-as-string=${this.INT_AS_STRING}`;
 
   getContract(contractAddress: ContractAddress): Promise<Contract> {
     return this.get<Contract>(
-      `/v2/contracts/${contractAddress}?${this.params}`,
+      `/v2/contracts/${contractAddress}?${this.defaultParams}`,
     );
   }
-  getContractLogs(contractAddress: ContractAddress): Promise<ContractLog[]> {
+
+  getAllContractLogs(contractAddress: ContractAddress): Promise<ContractLog[]> {
     return this.getAllPages<ContractLog>(
-      `/v2/contracts/logs?contract_id=${contractAddress}&${this.params}`,
+      `/v2/contracts/logs?contract_id=${contractAddress}&${this.defaultParams}`,
+    );
+  }
+
+  getContractLogsUntilCondition(
+    condition: (contractLog: ContractLog) => boolean,
+    contractAddress: ContractAddress,
+  ): Promise<ContractLog[]> {
+    return this.getPagesUntilCondition<ContractLog>(
+      condition,
+      `/v2/contracts/logs?contract_id=${contractAddress}&direction=backward&limit=${this.LIMIT}&int-as-string=${this.INT_AS_STRING}`,
     );
   }
 
@@ -39,13 +50,13 @@ export class MdwClientService {
     microBlockHash: string,
   ): Promise<AccountBalance> {
     return this.get<AccountBalance>(
-      `/v2/aex9/${contractAddress}/balances/${accountAddress}?hash=${microBlockHash}&${this.params}`,
+      `/v2/aex9/${contractAddress}/balances/${accountAddress}?hash=${microBlockHash}&${this.defaultParams}`,
     );
   }
 
   getMicroBlock(microBlockHash: MicroBlockHash): Promise<MdwMicroBlock> {
     return this.get<MdwMicroBlock>(
-      `/v2/micro-blocks/${microBlockHash}?${this.params}`,
+      `/v2/micro-blocks/${microBlockHash}?${this.defaultParams}`,
     );
   }
 
@@ -54,7 +65,7 @@ export class MdwClientService {
     microBlockHash: MicroBlockHash,
   ): Promise<BalancesV1> {
     return this.get<BalancesV1>(
-      `/aex9/balances/hash/${microBlockHash}/${contractAddress}?${this.params}`,
+      `/aex9/balances/hash/${microBlockHash}/${contractAddress}?${this.defaultParams}`,
     );
   }
 
@@ -71,19 +82,23 @@ export class MdwClientService {
     });
   }
 
+  // Fetches pages from middleware until the page contains at least one entry that meets the condition
+  private async getPagesUntilCondition<T>(
+    condition: (data: T) => boolean,
+    next: string,
+  ): Promise<T[]> {
+    const result = await this.get<MdwPaginatedResponse<T>>(next);
+
+    if (result.data.filter(condition).length === 0 && result.next) {
+      return result.data.concat(
+        await this.getPagesUntilCondition<T>(condition, result.next),
+      );
+    }
+    return result.data;
+  }
+
   private async getAllPages<T>(next: string): Promise<T[]> {
-    const url = `${NETWORKS[nonNullable(process.env.NETWORK_NAME)].middlewareHttpUrl}${next}`;
-    const result: MdwPaginatedResponse<T> = await fetch(url).then(
-      async (res) => {
-        if (res.ok) {
-          return (await res.json()) as Promise<MdwPaginatedResponse<T>>;
-        } else {
-          throw new Error(
-            `GET ${url} failed with status ${res.status}. Response body: ${await res.text()}`,
-          );
-        }
-      },
-    );
+    const result = await this.get<MdwPaginatedResponse<T>>(next);
 
     if (result.next) {
       return result.data.concat(await this.getAllPages(result.next));
