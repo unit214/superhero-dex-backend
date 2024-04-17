0b68c0d38bdafcb06bca1b42da1dbc8eca50d707
Timo Erdelt <timo@unit214.de>
2024-04-17 11:42:20 +0200

    refactor: improve imports in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import { AppModule } from './app.module';
-import { nonNullable } from './lib/utils';
-import { PairSyncService } from './tasks/pair-sync/pair-sync.service';
+import { AppModule } from '@/app.module';
+import { nonNullable } from '@/lib/utils';
+import { PairSyncService } from '@/tasks/pair-sync/pair-sync.service';
 
 const version = nonNullable(process.env.npm_package_version);
 async function bootstrap() {
