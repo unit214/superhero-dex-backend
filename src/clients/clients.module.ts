37e099ac08a75b81def21183d9d3920ba8131358
Timo Erdelt <timo@unit214.de>
2024-04-24 15:53:55 +0200

    feat: implemented fetching of fiat price via coinmarketcap api

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 
+import { CoinmarketcapClientService } from '@/clients/coinmarketcap-client.service';
+import { HttpService } from '@/clients/http.service';
 import { MdwHttpClientService } from '@/clients/mdw-http-client.service';
 import { MdwWsClientService } from '@/clients/mdw-ws-client.service';
 import { SdkClientService } from '@/clients/sdk-client.service';
 
 @Module({
-  providers: [MdwHttpClientService, MdwWsClientService, SdkClientService],
-  exports: [MdwHttpClientService, MdwWsClientService, SdkClientService],
+  providers: [
+    CoinmarketcapClientService,
+    HttpService,
+    MdwHttpClientService,
+    MdwWsClientService,
+    SdkClientService,
+  ],
+  exports: [
+    CoinmarketcapClientService,
+    HttpService,
+    MdwHttpClientService,
+    MdwWsClientService,
+    SdkClientService,
+  ],
 })
 export class ClientsModule {}
