cab8b70b8852b148855edd2b0b68afd59ca807b4
Keno Dressel <keno@unit214.de>
2024-06-11 12:28:57 +0200

    feat: adds sync algorithm to path calculator

--- src/tasks/pair-path-calculator/pair-path-calculator.service.ts
+++ src/tasks/pair-path-calculator/pair-path-calculator.service.ts
@@ -1,135 +1,134 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { groupBy, last, map } from 'lodash';
 
-import { MdwHttpClientService } from '@/clients/mdw-http-client.service';
-import {
-  ContractAddress,
-  contractAddrToAccountAddr,
-  MicroBlockHash,
-} from '@/clients/sdk-client.model';
-import { SdkClientService } from '@/clients/sdk-client.service';
-import { PairDbService } from '@/database/pair/pair-db.service';
 import { PairLiquidityInfoHistoryDbService } from '@/database/pair-liquidity-info-history/pair-liquidity-info-history-db.service';
+import { TokenDbService } from '@/database/token/token-db.service';
 import { getPaths } from '@/lib/paths';
-import { decimalToBigInt } from '@/lib/utils';
+import { decimalToBigInt, numberToDecimal } from '@/lib/utils';
 
 @Injectable()
 export class PairPathCalculatorService {
   constructor(
     private pairLiquidityInfoHistoryDb: PairLiquidityInfoHistoryDbService,
-    private pairDb: PairDbService,
-    private mdwClient: MdwHttpClientService,
-    private sdkClient: SdkClientService,
+    private tokenDb: TokenDbService,
   ) {}
 
   readonly logger = new Logger(PairPathCalculatorService.name);
-
-  readonly VALIDATION_WINDOW_BLOCKS = 20;
 
   async sync() {
     // get all history entries from the info history db service
     const entries =
       await this.pairLiquidityInfoHistoryDb.getEntriesWithoutAePrice();
 
+    this.logger.log(`Found ${entries.length} entries without AE price`);
+
+    const waeToken = await this.tokenDb.getByAddress(process.env.WAE_ADDRESS!);
+    if (!waeToken) {
+      throw new Error('WAE token not found in db');
+    }
+
+    const precisionDenominator = 10n ** 18n;
+    const poolFee = 997n * (precisionDenominator / 1000n);
+
     for (const entry of entries) {
+      this.logger.debug(`Processing entry ${entry.id}`);
       // for each entry get the timestamp and use the info history db service to fetch the graph at the time
       const graph =
         await this.pairLiquidityInfoHistoryDb.getLatestEntryForAllPairsAtTime(
           entry.microBlockTime,
         );
+      // build the graph and find the shortest path by using the path calculator
       const edges = graph.map((data) => ({
         data,
         t0: data.t0,
         t1: data.t1,
       }));
-      const shortestPath = getPaths(entry.pair.t0, entry.pair.t1, edges).reduce(
-        (a, b) => (a.length <= b.length ? a : b),
-      );
-    }
 
-    // build the graph and find the shortest path by using the path calculator
-    // if we find a path calculate the exchange rate to ae at the time and store it
-    // if we don't find a path store a null value
-  }
+      const aePrices = [entry.pair.t0, entry.pair.t1].map((token) => {
+        // validate if the token is the wae token, then we can skip the path calculation
+        if (token === waeToken.id) {
+          return 1n;
+        }
 
-  async validate() {
-    this.logger.log(`Started validating pair liquidity info history.`);
+        // get all paths
+        const paths = getPaths(token, waeToken.id, edges);
+        this.logger.debug(`Found ${paths.length} paths from ${token} to WAE`);
 
-    // Get current height
-    const currentHeight = await this.sdkClient.getHeight();
+        // if there are no paths we can't calculate the exchange rate
+        // TODO determine how we flag this so the calculation will not be repeated
+        if (paths.length === 0) {
+          return null;
+        }
 
-    // Get all liquidity entries greater or equal the current height minus VALIDATION_WINDOW_BLOCKS
-    // and take the last entry of every microBlock to get the final reserve in that microBlock
-    const liquidityEntriesWithinHeightSorted = map(
-      groupBy(
-        await this.pairLiquidityInfoHistoryDb.getWithinHeightSortedWithPair(
-          currentHeight - this.VALIDATION_WINDOW_BLOCKS,
-        ),
-        'microBlockHash',
-      ),
-      (group) => last(group)!,
-    );
-
-    // If the reserves of a local microBlock do not match with the data from the middleware or the block does not exist,
-    // delete all logs in this block and all newer entries
-    let numDeleted = 0;
-    for (const liquidityEntry of liquidityEntriesWithinHeightSorted) {
-      let isError = false;
-      let mdwReserve0: bigint | undefined;
-      let mdwReserve1: bigint | undefined;
-
-      try {
-        // reserve0 is the balance of the pair contract's account of token0
-        mdwReserve0 = BigInt(
-          (
-            await this.mdwClient.getAccountBalanceForContractAtMicroBlockHash(
-              liquidityEntry.pair.token0.address as ContractAddress,
-              contractAddrToAccountAddr(
-                liquidityEntry.pair.address as ContractAddress,
-              ),
-              liquidityEntry.microBlockHash as MicroBlockHash,
-            )
-          ).amount,
+        // TODO check what the result is when there is no path from getPaths
+        const shortestPath = paths.reduce((a, b) =>
+          a.length <= b.length ? a : b,
         );
+        this.logger.debug(`Shortest path has ${shortestPath.length} edges`);
 
-        // reserve1 is the balance of the pair contract's account of token1
-        mdwReserve1 = BigInt(
-          (
-            await this.mdwClient.getAccountBalanceForContractAtMicroBlockHash(
-              liquidityEntry.pair.token1.address as ContractAddress,
-              contractAddrToAccountAddr(
-                liquidityEntry.pair.address as ContractAddress,
-              ),
-              liquidityEntry.microBlockHash as MicroBlockHash,
-            )
-          ).amount,
-        );
-      } catch (e) {
-        this.logger.error(e);
-        isError = true;
-      }
-      if (
-        isError ||
-        decimalToBigInt(liquidityEntry.reserve0) !== mdwReserve0 ||
-        decimalToBigInt(liquidityEntry.reserve1) !== mdwReserve1
-      ) {
-        numDeleted = (
-          await this.pairLiquidityInfoHistoryDb.deleteFromMicroBlockTime(
-            liquidityEntry.microBlockTime,
-          )
-        ).count;
-        break;
-      }
+        // if we find a path calculate the exchange rate to ae at the time and store it
+        if (shortestPath.length > 0) {
+          // now we have the nodes, but we need to know the direction (reserve0 -> reserve1 or reserve1 -> reserve0)
+          let previousToken = token;
+          return shortestPath.reduce((acc, edge) => {
+            // first edge we compare against our original token
+            // after that we compare to the previous token
+            // if the previous token is t0
+            // example: t0 = 10, t1 = 3
+            // how many t1 can we get for 1 t0
+            // 3 / 10 = 0.3 --> t1 / t0 = exchange rate
+
+            // next pair, t0 is now 3 and t1 is 10
+            // our token is t0 again, t1 is WAE
+            // how many t1 can we get for 1 t0
+            // 10 / 3 = 3.33 --> t1 / t0 = exchange rate
+
+            // now we combine the two exchange rates
+            // 3.33 * 0.3 = 1
+
+            // but we also have to pay a fee, so we have to subtract the fee from each step
+            // 3.33 * 0.3 * 0.997^2 = 0.9901
+
+            if (previousToken === edge.t0) {
+              previousToken = edge.t1;
+              return (
+                acc *
+                (decimalToBigInt(edge.reserve1) /
+                  decimalToBigInt(edge.reserve0)) *
+                poolFee
+              );
+            }
+            if (previousToken === edge.t1) {
+              previousToken = edge.t0;
+              return (
+                acc *
+                (decimalToBigInt(edge.reserve0) /
+                  decimalToBigInt(edge.reserve1)) *
+                poolFee
+              );
+            }
+            throw new Error(
+              'Could not match previous token with edge t0 or t1',
+            );
+          }, 1n * precisionDenominator);
+        }
+        return null;
+      });
+
+      // save the ae prices to the db
+      await this.pairLiquidityInfoHistoryDb.update(entry.id, {
+        token0AePrice:
+          aePrices[0] !== null
+            ? numberToDecimal(
+                Number(aePrices[0]) / Number(precisionDenominator),
+              )
+            : null,
+        token1AePrice:
+          aePrices[1] !== null
+            ? numberToDecimal(
+                Number(aePrices[1]) / Number(precisionDenominator),
+              )
+            : null,
+      });
     }
-
-    if (numDeleted > 0) {
-      this.logger.log(
-        `Found an inconsistency in pair liquidity info history. Deleted ${numDeleted} entries.`,
-      );
-    } else {
-      this.logger.log('No problems in pair liquidity info history found.');
-    }
-
-    this.logger.log('Finished validating pair liquidity info history.');
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 
 import { PairLiquidityInfoHistoryImporterService } from '@/tasks/pair-liquidity-info-history-importer/pair-liquidity-info-history-importer.service';
 import { PairLiquidityInfoHistoryValidatorService } from '@/tasks/pair-liquidity-info-history-validator/pair-liquidity-info-history-validator.service';
+import { PairPathCalculatorService } from '@/tasks/pair-path-calculator/pair-path-calculator.service';
 
 const EVERY_5_MINUTES_STARTING_AT_02_30 = '30 2-57/5 * * * *';
 
@@ -11,6 +12,7 @@ export class TasksService {
   constructor(
     private pairLiquidityInfoHistoryImporterService: PairLiquidityInfoHistoryImporterService,
     private pairLiquidityInfoHistoryValidatorService: PairLiquidityInfoHistoryValidatorService,
+    private pairPathCalculatorService: PairPathCalculatorService,
   ) {}
 
   private _isRunning = false;
@@ -34,6 +36,23 @@ export class TasksService {
     } catch (error) {
       this.pairLiquidityInfoHistoryImporterService.logger.error(
         `Import failed. ${error}`,
+      );
+      this.setIsRunning(false);
+    }
+  }
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async runPairLiquidityInfoHistoryExchangeRateCalculator() {
+    try {
+      if (!this.isRunning) {
+        this.setIsRunning(true);
+        await this.pairPathCalculatorService.sync();
+        this.setIsRunning(false);
+      }
+    } catch (error) {
+      console.error(error);
+      this.pairPathCalculatorService.logger.error(
+        `Path Calculation failed. ${error}`,
       );
       this.setIsRunning(false);
     }
