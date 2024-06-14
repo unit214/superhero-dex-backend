513d336f472a014838501ad64931d4bb82e75e05
Keno Dressel <hire@keno.digital>
2024-06-14 15:39:56 +0200

    feat(history): adds history query by token

--- src/api/pair-liquidity-info-history/pair-liquidity-info-history.controller.ts
+++ src/api/pair-liquidity-info-history/pair-liquidity-info-history.controller.ts
@@ -9,17 +9,18 @@ import { ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import BigNumber from 'bignumber.js';
 
 import { OrderQueryEnum, PairLiquidityInfoHistoryEntry } from '@/api/api.model';
+import { PairLiquidityInfoHistoryWithTokens } from '@/api/pair-liquidity-info-history/pair-liquidity-info-history.model';
 import { PairLiquidityInfoHistoryService } from '@/api/pair-liquidity-info-history/pair-liquidity-info-history.service';
 import { ContractAddress } from '@/clients/sdk-client.model';
 import { calculateUsdValue } from '@/lib/utils';
 
-@Controller('history/liquidity')
+@Controller('history')
 export class PairLiquidityInfoHistoryController {
   constructor(
     private readonly pairLiquidityInfoHistoryService: PairLiquidityInfoHistoryService,
   ) {}
 
-  @Get()
+  @Get('')
   @ApiOperation({
     summary: 'Retrieve all entries of the pair liquidity info history',
   })
@@ -49,6 +50,12 @@ export class PairLiquidityInfoHistoryController {
     required: false,
   })
   @ApiQuery({
+    name: 'tokenAddress',
+    type: String,
+    description: 'Retrieve only history entries for the given token address',
+    required: false,
+  })
+  @ApiQuery({
     name: 'height',
     type: Number,
     description: 'Retrieve only history entries for the given height',
@@ -75,89 +82,90 @@ export class PairLiquidityInfoHistoryController {
     @Query('order', new ParseEnumPipe(OrderQueryEnum, { optional: true }))
     order: OrderQueryEnum = OrderQueryEnum.asc,
     @Query('pairAddress') pairAddress?: ContractAddress,
+    @Query('tokenAddress') tokenAddress?: ContractAddress,
     @Query('height', new ParseIntPipe({ optional: true })) height?: number,
     @Query('fromBlockTime', new ParseIntPipe({ optional: true }))
     fromBlockTime?: number,
     @Query('toBlockTime', new ParseIntPipe({ optional: true }))
     toBlockTime?: number,
   ): Promise<PairLiquidityInfoHistoryEntry[]> {
-    return this.pairLiquidityInfoHistoryService
-      .getAllHistoryEntries(
-        Number(limit),
-        Number(offset),
+    const historyEntries =
+      await this.pairLiquidityInfoHistoryService.getAllHistoryEntries({
+        limit: Number(limit),
+        offset: Number(offset),
         order,
         pairAddress,
-        height != null ? Number(height) : undefined,
-        fromBlockTime != null ? BigInt(fromBlockTime) : undefined,
-        toBlockTime != null ? BigInt(toBlockTime) : undefined,
-      )
-      .then((entries) =>
-        entries.map((entry) => {
-          const usdItems: {
-            reserveUsd: string | null;
-            txUsdFee: string | null;
-            txUsdValue: string | null;
-          } = {
-            reserveUsd: null,
-            txUsdFee: null,
-            txUsdValue: null,
-          };
-          const token0AePrice =
-            entry.token0AePrice === null ||
-            entry.token0AePrice.toString() === '-1'
-              ? null
-              : entry.token0AePrice?.toString();
+        tokenAddress,
+        height: height != null ? Number(height) : undefined,
+        fromBlockTime:
+          fromBlockTime != null ? BigInt(fromBlockTime) : undefined,
+        toBlockTime: toBlockTime != null ? BigInt(toBlockTime) : undefined,
+      });
+    return historyEntries.map(this.mapDbEntryToResponse);
+  }
 
-          const token1AePrice =
-            entry.token1AePrice === null ||
-            entry.token1AePrice.toString() === '-1'
-              ? null
-              : entry.token1AePrice?.toString();
+  private mapDbEntryToResponse(entry: PairLiquidityInfoHistoryWithTokens) {
+    const usdItems: {
+      reserveUsd: string | null;
+      txUsdFee: string | null;
+      txUsdValue: string | null;
+    } = {
+      reserveUsd: null,
+      txUsdFee: null,
+      txUsdValue: null,
+    };
+    const token0AePrice =
+      entry.token0AePrice === null || entry.token0AePrice.toString() === '-1'
+        ? null
+        : entry.token0AePrice?.toString();
 
-          if (token0AePrice !== null && token1AePrice !== null) {
-            usdItems.reserveUsd = calculateUsdValue({
-              reserve0: entry.reserve0.toString(),
-              reserve1: entry.reserve1.toString(),
-              token0AePrice: token0AePrice,
-              token1AePrice: token1AePrice,
-              decimals0: entry.pair.token0.decimals,
-              decimals1: entry.pair.token1.decimals,
-              aeUsdPrice: entry.aeUsdPrice.toString(),
-            });
-            usdItems.txUsdValue = calculateUsdValue({
-              reserve0: entry.deltaReserve0.toString(),
-              reserve1: entry.deltaReserve1.toString(),
-              token0AePrice: token0AePrice,
-              token1AePrice: token1AePrice,
-              decimals0: entry.pair.token0.decimals,
-              decimals1: entry.pair.token1.decimals,
-              aeUsdPrice: entry.aeUsdPrice.toString(),
-            });
-            usdItems.txUsdFee = new BigNumber(usdItems.txUsdValue)
-              .multipliedBy(0.003)
-              .toString();
-          }
+    const token1AePrice =
+      entry.token1AePrice === null || entry.token1AePrice.toString() === '-1'
+        ? null
+        : entry.token1AePrice?.toString();
 
-          return {
-            pairAddress: entry.pair.address,
-            senderAccount: entry.senderAccount,
-            type: entry.eventType,
-            reserve0: entry.reserve0.toString(),
-            reserve1: entry.reserve1.toString(),
-            deltaReserve0: entry.deltaReserve0.toString(),
-            deltaReserve1: entry.deltaReserve1.toString(),
-            token0AePrice: token0AePrice,
-            token1AePrice: token1AePrice,
-            aeUsdPrice: entry.aeUsdPrice.toString(),
-            height: entry.height,
-            microBlockHash: entry.microBlockHash,
-            microBlockTime: entry.microBlockTime.toString(),
-            transactionHash: entry.transactionHash,
-            transactionIndex: entry.transactionIndex.toString(),
-            logIndex: entry.logIndex,
-            ...usdItems,
-          };
-        }),
-      );
+    if (token0AePrice !== null && token1AePrice !== null) {
+      usdItems.reserveUsd = calculateUsdValue({
+        reserve0: entry.reserve0.toString(),
+        reserve1: entry.reserve1.toString(),
+        token0AePrice: token0AePrice,
+        token1AePrice: token1AePrice,
+        decimals0: entry.pair.token0.decimals,
+        decimals1: entry.pair.token1.decimals,
+        aeUsdPrice: entry.aeUsdPrice.toString(),
+      });
+      usdItems.txUsdValue = calculateUsdValue({
+        reserve0: entry.deltaReserve0.toString(),
+        reserve1: entry.deltaReserve1.toString(),
+        token0AePrice: token0AePrice,
+        token1AePrice: token1AePrice,
+        decimals0: entry.pair.token0.decimals,
+        decimals1: entry.pair.token1.decimals,
+        aeUsdPrice: entry.aeUsdPrice.toString(),
+      });
+      usdItems.txUsdFee = new BigNumber(usdItems.txUsdValue)
+        .multipliedBy(0.003)
+        .toString();
+    }
+
+    return {
+      pairAddress: entry.pair.address,
+      senderAccount: entry.senderAccount,
+      type: entry.eventType,
+      reserve0: entry.reserve0.toString(),
+      reserve1: entry.reserve1.toString(),
+      deltaReserve0: entry.deltaReserve0.toString(),
+      deltaReserve1: entry.deltaReserve1.toString(),
+      token0AePrice: token0AePrice,
+      token1AePrice: token1AePrice,
+      aeUsdPrice: entry.aeUsdPrice.toString(),
+      height: entry.height,
+      microBlockHash: entry.microBlockHash,
+      microBlockTime: entry.microBlockTime.toString(),
+      transactionHash: entry.transactionHash,
+      transactionIndex: entry.transactionIndex.toString(),
+      logIndex: entry.logIndex,
+      ...usdItems,
+    };
   }
 }
