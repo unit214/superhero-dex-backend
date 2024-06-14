05b90c037677e102da809316a9167bdd60cb5e0d
Keno Dressel <hire@keno.digital>
2024-06-14 15:39:56 +0200

    feat(history): adds history query by token

--- src/api/pair-liquidity-info-history/pair-liquidity-info-history.service.ts
+++ src/api/pair-liquidity-info-history/pair-liquidity-info-history.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Pair, PairLiquidityInfoHistory, Token } from '@prisma/client';
 
-import { OrderQueryEnum } from '@/api/api.model';
-import { ContractAddress } from '@/clients/sdk-client.model';
 import { PairLiquidityInfoHistoryDbService } from '@/database/pair-liquidity-info-history/pair-liquidity-info-history-db.service';
 
 @Injectable()
@@ -10,27 +7,6 @@ export class PairLiquidityInfoHistoryService {
   constructor(
     private readonly pairLiquidityInfoHistoryDb: PairLiquidityInfoHistoryDbService,
   ) {}
-  getAllHistoryEntries(
-    limit: number,
-    offset: number,
-    order: OrderQueryEnum,
-    pairAddress?: ContractAddress,
-    height?: number,
-    fromBlockTime?: bigint,
-    toBlockTime?: bigint,
-  ): Promise<
-    ({
-      pair: Pair & { token0: Token; token1: Token };
-    } & PairLiquidityInfoHistory)[]
-  > {
-    return this.pairLiquidityInfoHistoryDb.getAll(
-      limit,
-      offset,
-      order,
-      pairAddress,
-      height,
-      fromBlockTime,
-      toBlockTime,
-    );
-  }
+
+  getAllHistoryEntries = this.pairLiquidityInfoHistoryDb.getAll;
 }
