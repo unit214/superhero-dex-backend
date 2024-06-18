3d76c9d92938cc2d40103f80e6ed8d64ce67212b
Philipp <github@piwo.space>
2024-06-18 13:50:06 +0300

    fix: linter

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,9 +5,9 @@ import { ClientsModule } from '@/clients/clients.module';
 import { DatabaseModule } from '@/database/database.module';
 import { PairLiquidityInfoHistoryImporterService } from '@/tasks/pair-liquidity-info-history-importer/pair-liquidity-info-history-importer.service';
 import { PairLiquidityInfoHistoryValidatorService } from '@/tasks/pair-liquidity-info-history-validator/pair-liquidity-info-history-validator.service';
+import { PairPathCalculatorService } from '@/tasks/pair-path-calculator/pair-path-calculator.service';
 import { PairSyncService } from '@/tasks/pair-sync/pair-sync.service';
 import { TasksService } from '@/tasks/tasks.service';
-import { PairPathCalculatorService } from '@/tasks/pair-path-calculator/pair-path-calculator.service';
 
 @Module({
   imports: [ClientsModule, DatabaseModule, ScheduleModule.forRoot()],
