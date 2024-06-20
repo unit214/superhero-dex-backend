6d620c0eff422c16934cf103019975dfc9031080
Philipp <github@piwo.space>
2024-06-20 09:11:52 +0300

    refactor: simplify total reserve function

--- prisma/migrations/20240619141013_add_total_reserve_function/migration.sql
+++ prisma/migrations/20240619141013_add_total_reserve_function/migration.sql
@@ -1,23 +1,17 @@
-CREATE FUNCTION total_reserve (integer) RETURNS numeric AS 'SELECT COALESCE((SELECT COALESCE(SUM(latest_liquidity_info_t0.reserve0 / POW(10, t0.decimals)),
-                                  0) as total_reserve
-                  FROM "Token" t0
-                           LEFT JOIN public."Pair" pt0 on t0.id = pt0.t0
-                           LEFT JOIN LATERAL (SELECT *
-                                              FROM "PairLiquidityInfoHistory"
-                                              WHERE pt0.id = "pairId"
-                                              ORDER BY "microBlockTime" DESC, "logIndex" DESC
-                                              LIMIT 1) latest_liquidity_info_t0 ON TRUE
-                  WHERE $1 = t0.id), 0) +
-        COALESCE((SELECT COALESCE(SUM(latest_liquidity_info_t1.reserve1 / POW(10, t1.decimals)),
-                                  0) as total_reserve
-                  FROM "Token" t1
-                           LEFT JOIN public."Pair" pt1 on t1.id = pt1.t1
-                           LEFT JOIN LATERAL (SELECT *
-                                              FROM "PairLiquidityInfoHistory"
-                                              WHERE pt1.id = "pairId"
-                                              ORDER BY "microBlockTime" DESC, "logIndex" DESC
-                                              LIMIT 1) latest_liquidity_info_t1 ON TRUE
-                  WHERE $1 = t1.id), 0)' LANGUAGE SQL IMMUTABLE RETURNS NULL ON NULL INPUT;
+CREATE
+OR REPLACE FUNCTION total_reserve (integer) RETURNS numeric AS 'SELECT SUM(CASE
+                WHEN t.id = p.t0 THEN
+                    (latest_liquidity_info."reserve0" / POW(10, t.decimals))
+                ELSE (latest_liquidity_info."reserve1" / POW(10, t.decimals)) END
+        )
+ FROM "Token" t
+          LEFT JOIN public."Pair" p on t.id = p.t0 OR t.id = p.t1
+          LEFT JOIN LATERAL (SELECT *
+                             FROM "PairLiquidityInfoHistory"
+                             WHERE p.id = "pairId"
+                             ORDER BY "microBlockTime" DESC, "logIndex" DESC
+                             LIMIT 1) latest_liquidity_info ON TRUE
+ WHERE $1 = t.id' LANGUAGE SQL IMMUTABLE RETURNS NULL ON NULL INPUT;
 
 CREATE
 OR REPLACE FUNCTION volume_usd (integer, interval) RETURNS numeric AS 'SELECT ROUND(SUM(CASE
