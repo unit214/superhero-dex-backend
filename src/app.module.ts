7ffdcf00f4b3b8c8b779f88d88b770a2f6025af1
Keno Dressel <keno@unit214.de>
2024-06-21 16:50:46 +0200

    feat: triggers history sync on new tx

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,22 @@ import { AppController } from '@/app.controller';
 import { ClientsModule } from '@/clients/clients.module';
 import { MdwWsClientService } from '@/clients/mdw-ws-client.service';
 import { DatabaseModule } from '@/database/database.module';
+import { PairLiquidityInfoHistoryImporterService } from '@/tasks/pair-liquidity-info-history-importer/pair-liquidity-info-history-importer.service';
+import { PairPathCalculatorService } from '@/tasks/pair-path-calculator/pair-path-calculator.service';
 import { PairSyncService } from '@/tasks/pair-sync/pair-sync.service';
 import { TasksModule } from '@/tasks/tasks.module';
 
 @Module({
   imports: [ApiModule, ClientsModule, DatabaseModule, TasksModule],
   controllers: [AppController],
-  providers: [MdwWsClientService, PairsService, TokensService, PairSyncService],
+  providers: [
+    MdwWsClientService,
+    PairsService,
+    TokensService,
+    PairSyncService,
+    // FIXME probably not the right place but did not get it to work any otherway
+    PairLiquidityInfoHistoryImporterService,
+    PairPathCalculatorService,
+  ],
 })
 export class AppModule {}
