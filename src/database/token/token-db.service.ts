39ba83798285b34a48b2d23816f16655f4d1d6c0
Timo Erdelt <timo@unit214.de>
2024-08-13 11:51:05 +0200

    feat: change get token route to token/:address and include price data

--- src/database/token/token-db.service.ts
+++ src/database/token/token-db.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Token } from '@prisma/client';
+import { Prisma, Token } from '@prisma/client';
 
 import { ContractAddress } from '@/clients/sdk-client.model';
 import { PrismaService } from '@/database/prisma.service';
@@ -42,7 +42,150 @@ export class TokenDbService {
         priceChangeMonth: string;
         priceChangeYear: string;
       }[]
-    >`
+    >(this.getWithAggregationQuery(showInvalidTokens, null));
+  }
+
+  async getWithAggregation(address: string) {
+    return this.prisma
+      .$queryRaw<{
+        address: string;
+        symbol: string;
+        name: string;
+        decimals: number;
+        malformed: boolean;
+        noContract: boolean;
+        listed: boolean;
+        priceAe: string;
+        priceUsd: string;
+        tvlAe: string;
+        tvlUsd: string;
+        totalReserve: string;
+        pairs: number;
+        volumeUsdDay: string;
+        volumeUsdWeek: string;
+        volumeUsdMonth: string;
+        volumeUsdYear: string;
+        volumeUsdAll: string;
+        priceChangeDay: string;
+        priceChangeWeek: string;
+        priceChangeMonth: string;
+        priceChangeYear: string;
+      }>(this.getWithAggregationQuery(true, address))
+      .then((x) => x[0]);
+  }
+
+  getListed(): Promise<Token[]> {
+    //there is no reason to list invalid tokens
+    return this.prisma.token.findMany({
+      where: { ...validTokenCondition, listed: true },
+    });
+  }
+
+  getByAddress(address: string) {
+    return this.prisma.token.findFirst({
+      where: { address },
+    });
+  }
+
+  async updateListedValue(address: string, listed: boolean) {
+    //ensure the token is valid in order to be listed
+    if (listed) {
+      const exists = await this.prisma.token.findFirst({
+        where: { address },
+        select: { malformed: true, noContract: true },
+      });
+      if (exists?.malformed || exists?.noContract) {
+        throw new Error("An invalid token can't be listed");
+      }
+    }
+    return this.prisma.token.update({
+      //we don't want to list invalid tokens
+      where: { address },
+      data: { listed },
+    });
+  }
+
+  getByAddressWithPairs(address: string) {
+    return this.prisma.token.findFirst({
+      where: { address },
+      include: { pairs0: true, pairs1: true },
+    });
+  }
+
+  count(showInvalidTokens: boolean, onlyListed?: boolean) {
+    return this.prisma.token.count({
+      where: {
+        ...(onlyListed ? { listed: true } : {}),
+        ...(() => (showInvalidTokens ? {} : validTokenCondition))(),
+      },
+    });
+  }
+
+  getByAddressWithPairsAndLiquidity(address: string) {
+    return this.prisma.token.findFirst({
+      where: { address },
+      include: {
+        pairs0: { include: { token1: true, liquidityInfo: true } },
+        pairs1: { include: { token0: true, liquidityInfo: true } },
+      },
+    });
+  }
+
+  async getAllAddresses(
+    showInvalidTokens: boolean,
+  ): Promise<ContractAddress[]> {
+    return (
+      await this.prisma.token.findMany({
+        where: showInvalidTokens ? {} : validTokenCondition,
+        select: {
+          address: true,
+        },
+      })
+    ).map((x) => x.address as ContractAddress);
+  }
+
+  upsertToken(
+    address: string,
+    symbol: string,
+    name: string,
+    decimals: number,
+  ): Promise<Token> {
+    return this.commonUpsert(address, {
+      symbol,
+      name,
+      decimals,
+      noContract: false,
+      malformed: false,
+    });
+  }
+
+  upsertMalformedToken(address: string): Promise<Token> {
+    return this.commonUpsert(address, { malformed: true, noContract: false });
+  }
+
+  upsertNoContractForToken(address: string): Promise<Token> {
+    return this.commonUpsert(address, { malformed: false, noContract: true });
+  }
+
+  private commonUpsert(
+    address: string,
+    common: Partial<Token>,
+  ): Promise<Token> {
+    return this.prisma.token.upsert({
+      where: {
+        address,
+      },
+      update: common,
+      create: { address, ...common },
+    });
+  }
+
+  private getWithAggregationQuery(
+    showInvalidTokens: boolean,
+    tokenAddress: string | null,
+  ) {
+    const hasAddressFilter: boolean = tokenAddress !== null;
+    return Prisma.sql`
       SELECT
         t.address,
         t.symbol,
@@ -151,118 +294,22 @@ export class TokenDbService {
         ) latest_liquidity_info ON TRUE
       WHERE
         CASE
-          WHEN ${showInvalidTokens} THEN t.malformed = FALSE
+          WHEN NOT ${showInvalidTokens} THEN t.malformed = FALSE
           AND t."noContract" = FALSE
           ELSE TRUE
         END
+        AND
+        CASE
+           WHEN ${hasAddressFilter} THEN t.address = ${tokenAddress}
+           ELSE TRUE
+        END
       GROUP BY
         t.id
+      LIMIT
+      CASE
+        WHEN ${hasAddressFilter} THEN 1
+        ELSE NULL
+      END
     `;
-  }
-
-  getListed(): Promise<Token[]> {
-    //there is no reason to list invalid tokens
-    return this.prisma.token.findMany({
-      where: { ...validTokenCondition, listed: true },
-    });
-  }
-
-  getByAddress(address: string) {
-    return this.prisma.token.findFirst({
-      where: { address },
-    });
-  }
-
-  async updateListedValue(address: string, listed: boolean) {
-    //ensure the token is valid in order to be listed
-    if (listed) {
-      const exists = await this.prisma.token.findFirst({
-        where: { address },
-        select: { malformed: true, noContract: true },
-      });
-      if (exists?.malformed || exists?.noContract) {
-        throw new Error("An invalid token can't be listed");
-      }
-    }
-    return this.prisma.token.update({
-      //we don't want to list invalid tokens
-      where: { address },
-      data: { listed },
-    });
-  }
-
-  getByAddressWithPairs(address: string) {
-    return this.prisma.token.findFirst({
-      where: { address },
-      include: { pairs0: true, pairs1: true },
-    });
-  }
-
-  count(showInvalidTokens: boolean, onlyListed?: boolean) {
-    return this.prisma.token.count({
-      where: {
-        ...(onlyListed ? { listed: true } : {}),
-        ...(() => (showInvalidTokens ? {} : validTokenCondition))(),
-      },
-    });
-  }
-
-  getByAddressWithPairsAndLiquidity(address: string) {
-    return this.prisma.token.findFirst({
-      where: { address },
-      include: {
-        pairs0: { include: { token1: true, liquidityInfo: true } },
-        pairs1: { include: { token0: true, liquidityInfo: true } },
-      },
-    });
-  }
-
-  async getAllAddresses(
-    showInvalidTokens: boolean,
-  ): Promise<ContractAddress[]> {
-    return (
-      await this.prisma.token.findMany({
-        where: showInvalidTokens ? {} : validTokenCondition,
-        select: {
-          address: true,
-        },
-      })
-    ).map((x) => x.address as ContractAddress);
-  }
-
-  upsertToken(
-    address: string,
-    symbol: string,
-    name: string,
-    decimals: number,
-  ): Promise<Token> {
-    return this.commonUpsert(address, {
-      symbol,
-      name,
-      decimals,
-      noContract: false,
-      malformed: false,
-    });
-  }
-
-  upsertMalformedToken(address: string): Promise<Token> {
-    return this.commonUpsert(address, { malformed: true, noContract: false });
-  }
-
-  upsertNoContractForToken(address: string): Promise<Token> {
-    return this.commonUpsert(address, { malformed: false, noContract: true });
-  }
-
-  private commonUpsert(
-    address: string,
-    common: Partial<Token>,
-  ): Promise<Token> {
-    return this.prisma.token.upsert({
-      where: {
-        address,
-      },
-      update: common,
-      create: { address, ...common },
-    });
   }
 }
